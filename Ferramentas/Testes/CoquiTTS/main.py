6791c4d24f976710647970207291060ddcabff1e
Samuel Zaire Rocha <96780102+szaire@users.noreply.github.com>
2024-06-14 15:40:43 -0300

    update

---
+++ Ferramentas/Testes/CoquiTTS/main.py
@@ -0,0 +1,11 @@
+import torch
+from TTS.api import TTS
+
+# Get device
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+# List available 🐸TTS models
+print(TTS().list_models())
+
+tts = TTS(model_name="tts_models/multilingual/multi-dataset/your_tts", progress_bar=False).to(device)
+tts.tts_to_file("This is voice cloning.", speaker_wav="my/cloning/audio.wav", language="en", file_path="output.wav")
--- Ferramentas/Testes/GROQ/main.py
+++ Ferramentas/Testes/GROQ/main.py
@@ -5,5 +5,27 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+client = Groq(
+    api_key=os.getenv("GROQ_API_KEY")
+)
 
-client = Groq(api_key=os.getenv(GROQ_API_KEY))
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": "Hey, Groq, how you doing? What Word will I learn today?"
+        },
+        {
+            "role": "system",
+            "content": 
+                """Forneça frases simples no idioma em inglês. 
+                Quando explicar um termo em inglês, explique-o em português.
+                Forneça exemplos em inglês e sua tradução para o português .
+                Em todo final de texto, solicite que o usuário tente utilizar 
+                a palavra em uma frase simples."""
+        }
+    ],
+    model="llama3-70b-8192"
+)
+
+print(chat_completion.choices[0].message.content)
